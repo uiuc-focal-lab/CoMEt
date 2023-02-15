f442686f20631f917426bb78bd1f149c97831121
ishcha <isha4@illinois.edu>
2023-02-15 00:02:31 -0600

    corrected error in argparse

--- explainOneBasicBlock.py
+++ explainOneBasicBlock.py
@@ -11,11 +11,11 @@ from testing_models import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("code", type=str, description='code for input x86 assembly basic block')
-    parser.add_argument("cost_model", type=str, description='cost model whose throughput predictions are to be explained')
-    parser.add_argument("precision_threshold", type=float, description='precision threshold for the Anchors algorithm (default=0.82)')
-    parser.add_argument("perturbation_probability", type=float, description='probability parameter in perturbation model (default=0.5)')
-    parser.add_argument('-token', action='store_true', description='give explanations with token-level predicates')
+    parser.add_argument("code", type=str, help='code for input x86 assembly basic block')
+    parser.add_argument("cost_model", type=str, help='cost model whose throughput predictions are to be explained')
+    parser.add_argument("precision_threshold", type=float, help='precision threshold for the Anchors algorithm (default=0.82)')
+    parser.add_argument("perturbation_probability", type=float, help='probability parameter in perturbation model (default=0.5)')
+    parser.add_argument('-token', action='store_true', help='give explanations with token-level predicates')
     args = parser.parse_args()
 
     predicate_type = 'instruction'
